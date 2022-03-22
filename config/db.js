464ca491a1e58af12cbb3ddef938b810e3cc2e5d
ndeya97 <nadiop97@gmail.com>
2022-03-22 11:49:54 +0100

    correct db error remove useFindAndModify: false to db.js

--- config/db.js
+++ config/db.js
@@ -6,7 +6,6 @@ const connectDB = async () => {
         const conn = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            useFindAndModify: false,
         });
 
         console.log(`MongoDB Connected: ${conn.connection.host}`);
